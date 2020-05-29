87c78488804eaa265c0035aa30ddfdeb72c93806
Cristóbal Contreras <44170429+chrisloarryn@users.noreply.github.com>
2020-05-29 13:40:02 -0400

    nginx

--- pretty-calculator/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-
-const initialValue = '123'
-
-ReactDOM.render(<App initialValue={initialValue} />, document.getElementById('root'));
